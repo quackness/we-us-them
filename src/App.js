0fabccff85fcea98e84ce3af20e40245f8b3235a
Karolina Redden <dubajkaro@gmail.com>
2023-01-17 18:09:33 +0100

    add env example file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
 import Contacts from './components/Contacts';
 import NavBar from './components/NavBar';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
-        <NavBar />
-      
+          <NavBar />
           <Routes>
             <Route exact path="/" element={<Contacts />} />
-          
-         
-
-      
+            <Route exact path="/contact/:id" element={<ContactPage />} />
           </Routes>
         </>
       </Router>
